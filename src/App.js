88aef16070f843fc0c8e7b785a88b90e37a34237
Rafael Schug <rafaelschugbc@gmail.com>
2021-09-28 13:42:06 -0300

    fix: Provider import is now handled through index.js instead of App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import { Provider } from "react-redux";
-import store from "./store";
 import FruitsPage from './components/FruitsPage';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
+    <div className="App">
         <FruitsPage></FruitsPage>
-      </div>
-    </Provider>
+    </div>
   );
 }
 
